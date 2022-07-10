6708b6a9e16a2c22f02d0ccd742f6d9ae7005d9c
liuyunjs <429695034@qq.com>
2022-07-10 14:17:35 +0800

    feat: useSelected

--- library/AccordionContent.tsx
+++ library/AccordionContent.tsx
@@ -1,37 +1,13 @@
 import * as React from 'react';
-import { LegacyCollapsible, CollapsibleProps } from './LegacyCollapsible';
-import { Collapsible, CollapsibleV2Props } from './Collapsible';
-import { ManagerContext, AccordionKeyContext } from './AccordionContext';
+import { Collapsible, CollapsibleProps } from './Collapsible';
+import { useSelected } from './useSelected';
 
-export type AccordionContentProps =
-  | (Omit<CollapsibleProps, 'collapsed'> & { useV2?: false })
-  | (Omit<CollapsibleV2Props, 'collapsed'> & { useV2: boolean } & {});
-
-const useSelected = () => {
-  const manager = React.useContext(ManagerContext);
-  const key = React.useContext(AccordionKeyContext);
-  const [selected, setSelected] = React.useState(() => manager.isSelected(key));
-
-  React.useEffect(() => {
-    const subscribe = () => {
-      setSelected(manager.isSelected(key));
-    };
-
-    manager.on('update', subscribe);
-
-    return () => {
-      manager.off('update', subscribe);
-    };
-  }, [key, manager]);
-
-  return selected;
-};
+export type AccordionContentProps = Omit<CollapsibleProps, 'collapsed'>;
 
 export const AccordionContent: React.FC<
   React.PropsWithChildren<AccordionContentProps>
 > = (props) => {
   const selected = useSelected();
-  const Component = props.useV2 ? Collapsible : LegacyCollapsible;
 
-  return <Component {...(props as any)} collapsed={!selected} />;
+  return <Collapsible {...(props as any)} collapsed={!selected} />;
 };
--- library/main.ts
+++ library/main.ts
@@ -3,5 +3,5 @@ export * from './AccordionContent';
 export * from './AccordionPanel';
 export * from './AccordionProvider';
 export * from './AccordionList';
-export { LegacyCollapsible } from './LegacyCollapsible';
 export { Collapsible } from './Collapsible';
+export { useSelected } from './useSelected';
